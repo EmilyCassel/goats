0c5e0b66423f92d927a61f76ffff1b35d159b197
emily <emilycassel77@gmail.com>
2024-03-18 15:33:42 -0400

    Initial commit

---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as Contact } from './Contact';
+export { default as Portfolio } from './Portfolio';
+export { default as About } from '../components/AboutMe';
+export { default as Resume } from './Resume';
