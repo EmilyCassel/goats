74c328487ea3edce1a31dfa49ec2429ed7199d76
emily <emilycassel77@gmail.com>
2024-04-06 12:19:04 -0400

    pipeline broken

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 function Home(){
     return(
-        <div>
+        <div className="homeBody">
             <h1 className="Welcome">Welcome to CU@LilRedBarnGoats</h1>
             <p>Thank you for your interest in out Nigerian Dwarf Dairy Goat Herd.</p>
         </div>
