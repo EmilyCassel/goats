e2d6fd84869f08c5ff4849030ea1527751bb95a2
emily <emilycassel77@gmail.com>
2024-04-12 16:28:31 -0400

    new repo

--- client/src/pages/BreedingSchedule.jsx
+++ client/src/pages/BreedingSchedule.jsx
@@ -2,7 +2,7 @@ function BreedingSchedule(){
     return(
         <div>
             <h1>CU@LilRedBarn Breeding Schedule 2025</h1>
-            <h2>We are currently</h2>
+            <h2>We are currently working on our 2024 kidding. Our 2025 breeding schedule will be available in October 2024.</h2>
         </div>
     )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ function Home(){
         <div className="homeBody">
             <div className="wordingHomeBody">
                 <h1 className="Welcome">Welcome to CU@LilRedBarnGoats</h1>
-                <p >Thank you for your interest in out Nigerian Dwarf Dairy Goat Herd.</p>
+                <p className="thankYou">Thank you for your interest in out Nigerian Dwarf Dairy Goat Herd.</p>
             </div>
         </div>
     )
