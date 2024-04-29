e21ca8f5d73f89c42c0f6aa136edd875ed7cc380
emily <emilycassel77@gmail.com>
2024-04-29 16:15:17 -0400

    stuff

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,10 @@
-
 function Footer (){
     return(
         <footer>
             
             <div className="footer">
-                <p>CU@LilRedBarn was sold in 2022. This website is a project completed with permission by the farms owners. No animals are up for purchase.</p>
-                <p>2024 Emily Cassel. All rights reserved</p>
+                <p className="sold">CU@LilRedBarn was sold in 2022. This website is a project completed with permission by the farms owners. No animals are up for purchase.</p>
+                <p className="rightsReserve">2024 Emily Cassel. All rights reserved</p>
             </div>
             
         </footer>
